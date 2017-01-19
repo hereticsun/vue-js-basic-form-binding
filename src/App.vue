246e6552c6a86ea3244cc0dd72e43201a62c9c61
Azlan Cuttilan <azlan@bynd.com>
2017-01-19 16:16:17 +0000

    Creating a custom control (input)
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/App.vue
+++ src/App.vue
@@ -94,6 +94,11 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -121,7 +126,7 @@
                         </ul>
                         <p>Gender: {{ gender }}</p>
                         <p>Priority: {{ selectedPriority }}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -130,6 +135,8 @@
 </template>
 
 <script>
+    import Switch from './Switch.vue';
+
     export default {
         data() {
             return {
@@ -142,9 +149,13 @@
                 sendMail: [],
                 gender: 'Male',
                 priorities: ['High', 'Medium', 'Low'],
-                selectedPriority: 'High'
+                selectedPriority: 'High',
+                dataSwitch: true
             }
         },
+        components: {
+            appSwitch: Switch
+        }
     }
 </script>
 
